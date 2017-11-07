91e3e64c18192eed0ea9fa558ec865baa7ca17f7
NA-Dev <n.aaron.na@gmail.com>
2017-11-07 15:40:44 -0500

    made code better, added win/lose conditions

--- assets/hangman.js
+++ assets/hangman.js
@@ -2,69 +2,118 @@
 // Nikki Aaron
 // 11/5/17
 
+var allPhrases, abc, guessedLet, unguessedLet, attempts, phrase, phraseHidden, textInput, letter;
 
-// starting phrase options and alphabet
-var allPhrases = ['CATCH SOME RAYS', 'HANG TEN', 'WALK ALONG THE SHORE', 'CRASHING WAVES', 'SEAFOAM GREEN', 'CONCH SHELL', 'NAUTILUS SHELL', 'BIKINIS AND TRUNKS', 'BUILD A SANDCASTLE', 'APPLY SUNSCREEN', 'WEAR YOUR FLIP-FLOPS', 'SCUBA WITH SHARKS', 'GROOVY MUSIC', 'BOARDWALK FISHING'];
+window.onload = setup();
 
-var unguessed = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+function setup() {
+    // starting phrase options and alphabet
+    allPhrases = ['CATCH SOME RAYS', 'HANG TEN', 'WALK ALONG THE SHORE', 'CRASHING WAVES', 'SEAFOAM GREEN', 'CONCH SHELL', 'NAUTILUS SHELL', 'BIKINIS AND TRUNKS', 'BUILD A SANDCASTLE', 'APPLY SUNSCREEN', 'WEAR YOUR FLIP-FLOPS', 'SCUBA WITH SHARKS', 'GROOVY MUSIC', 'BOARDWALK FISHING'];
+    abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    guessedLet = [];
+    unguessedLet = abc;
+    //start with 6 'lives' or attempts
+    //lose one attempt for each incorrect guess
+    attempts = 6;
+    //hide win and lose statements, show input
+    document.getElementById('lose').style.display = 'none';
+    document.getElementById('win').style.display = 'none';
+    document.getElementById('input').style.display = 'block';
+    console.log('setupExecuted');
+}
 
-var guessed = [];
-var imgMarginInit = 0;
+//select a random phrase from allPhrases
+phrase = allPhrases[Math.floor(Math.random()*(allPhrases.length - 1))]; //length or length-1?
+console.log(phrase);
 
-//generates a random phrase from allPhrases
-var random = Math.floor(Math.random()*(allPhrases.length-1));
-var phrase = allPhrases[random];
+//hide unguessed letters in phraseHidden
+phraseHidden = hide(phrase);
 
-
-//start with 6 "lives" or attempts
-//lose one attempt for each incorrect guess
-var attemptsRem = 6;
-
-window.onload = phraseGen();
-
-function phraseGen() {
-    //replaces unguessed letters in phrase with blanks
-    var phraseDisp = phrase;
-    for (var i = 0; i < phrase.length; i++) {
-        for (var j = 0; j < unguessed.length; j++) {
-            if (phrase.charAt(i) == unguessed[j]) {
-                phraseDisp = phraseDisp.replace(phraseDisp.charAt(i),'_');
+function hide(x) {
+    for (var i = 0; i < x.length; i++) {
+        for (var j = 0; j < unguessedLet.length; j++) {
+            if (x.charAt(i) == unguessedLet[j]) {
+                x = x.replace(x.charAt(i),'_');
             }
         }
     }
-    //displays resulting phraseDisp in HTML along with other initial vars
-    document.getElementById('phraseDisp').innerHTML = phraseDisp;
-    document.getElementById('attemptsRem').innerHTML = attemptsRem;
-    document.getElementById('guessed').innerHTML = guessed.join(', ');
-    document.getElementById('unguessed').innerHTML = unguessed.join(', ');
+    console.log('hideExecuted');
+    return x;
+}
+console.log(phraseHidden);
+
+refreshDisp();
+
+//refresh HTML
+function refreshDisp() {
+    document.getElementById('phraseDisp').innerHTML = phraseHidden;
+    document.getElementById('attemptsRem').innerHTML = attempts;
+    document.getElementById('guessed').innerHTML = guessedLet.join(', ');
+    document.getElementById('unguessed').innerHTML = unguessedLet.join(', ');
+    console.log('refreshDispExecuted');
 }
 
-//executes upon keyup within input form
+//execute validation upon keyup within HTML input form
 function validateLetter() {
-    var textInput = document.getElementById("newLetter").value;
+    textInput = document.getElementById('newLetter').value;
     textInput = textInput.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
-    document.getElementById("newLetter").value = textInput;
+    document.getElementById('newLetter').value = textInput;
+    console.log('validateLetter')
 }
 
-//executes upon click of form submit button
+//execute guess entry script upon click of HTML form submit button
 function newGuess() {
-    var newLetter = document.getElementById("newLetter").value;
-    const index1 = unguessed.indexOf(newLetter);
-    const index2 = guessed.indexOf(newLetter);
-    const index3 = phrase.indexOf(newLetter);
+    letter = document.getElementById('newLetter').value;
+    const index1 = unguessedLet.indexOf(letter);
+    const index2 = guessedLet.indexOf(letter);
+    const index3 = phrase.indexOf(letter);
+    //if the letter is a new guess
     if (index1 !== -1 && index2 == -1) {
-        unguessed.splice(index1,1);
-        guessed.push(newLetter);
+        console.log('new guess');
+        //then remove letter from unguessed, and add to guessed
+        unguessedLet.splice(index1,1);
+        guessedLet.push(letter);
+        //then, if the new letter is not in phrase, take away an attempt and go to next image frame
         if (index3 == -1) {
-            attemptsRem = attemptsRem - 1;
-            document.getElementById("hangman-img").style.marginLeft = "-" + (imgMarginInit + ((6 - attemptsRem)*100)) + "%";
+            console.log('notInPhrase');
+            attempts--;
+            document.getElementById('hangman-img').style.marginLeft = '-' + ((6 - attempts)*100) + '%';
+        } else {
+        //if the new letter is in the phrase, reveal it using hide() again
+
+           phraseHidden = hide(phrase);
+           console.log('inPhrase');
+           console.log(phraseHidden);
         }
+        console.log(attempts);
+    } else {
+        console.log('notNewGuess');
+        console.log(attempts);
     }
-    document.getElementById("newLetter").value = "";
-    phraseGen();
+    //clear letter from HTML form
+    document.getElementById('newLetter').value = '';
+    //refresh HTML displayed variables
+    refreshDisp();
+    console.log('newGuessExecuted')
+    checkWin();
 }
 
-function imgFrame() {
-
+function checkWin() {
+    const index = phraseHidden.indexOf('_');
+    if ((index !== -1) && (attempts == 0)) {
+        document.getElementById('phraseDisp').innerHTML = phrase;
+        document.getElementById('input').style.display = 'none';
+        document.getElementById('lose').style.display = 'block';
+    }
+    else if ((index == -1) && (attempts == 0)) {
+        document.getElementById('input').style.display = 'none';
+        document.getElementById('win').style.display = 'block';
+    }
+    else if ((index == -1) && (attempts > 0)) {
+        document.getElementById('input').style.display = 'none';
+        document.getElementById('win').style.display = 'block';
+    }
+    else {
+    }
 }
 
